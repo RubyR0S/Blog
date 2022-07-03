3d9a97cd5c7295e60dc526b8753b013c3aa0682c
RubyR0S <fedorovych22@ukr.net>
2022-07-03 14:35:51 +0300

    Implement articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,17 @@
 class ArticlesController < ApplicationController
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
     def new 
     end
 
     def create
-        @article = Article.new(article_params)
+        @article = Article. new(article_params)
         if @article.valid?
           @article.save
-          redirtect_to @article
+          redirect_to @article
         else
           render action: 'new'
         end 
