1b3103c73ee31f30ec50561a14d2c2f102fd2fa6
RubyR0S <fedorovych22@ukr.net>
2022-07-07 18:52:15 +0300

    Add usersname column to users table

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,4 @@
-class ComentsController < ApplicationController
+class CommentsController < ApplicationController
     def create
        @article = Article.find(params[:article_id])
        @article.comments.create(comment_params)
