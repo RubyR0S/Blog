7e0e31912ebbf6edc3bd19267fe0c6aa6182745c
RubyR0S <fedorovych22@ukr.net>
2022-07-13 22:59:42 +0300

    Add account creation test

---
+++ spec/features/visitor_creates_contact_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature "Acount Creation" do
+    scenario "allows gets to create account" do
+        visit new_user_registration_path
+
+        fill_in :user_email, :with => 'user@example.com'
+        fill_in :user_username, :with => 'mike'
+        fill_in :user_password, :with => 'secure123!@#'
+        fill_in :user_password_confirmation, :with => 'secure123!@#'
+
+        click_button 'Sign up'
+
+        expect(page).to have_content I18n.t('devise.registrations.signed_up')
+
+    end
+end
